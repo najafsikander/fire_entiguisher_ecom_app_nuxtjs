308e2e1507a94a0502a927f7abf6a9f4f2172c17
trotdrive <trotdrive@gmail.com>
2022-08-15 17:48:13 +0400

    first-commit

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="container">
+      <Hero />
+      <div class="rootLargeCard">
+        <LargeCardDisplay
+          v-for="cardInfo in largeCardSections"
+          :key="cardInfo.id"
+          :cardsSection="cardInfo"
+        />
+      </div>
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardSections"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ts-ignore
+import { largeCardSections, smallCardSections } from "@/assets/data.js";
+export default {
+  data() {
+    return {
+      largeCardSections: largeCardSections,
+      smallCardSections: smallCardSections,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.rootLargeCard {
+  margin-top: 20% !important;
+}
+</style>
